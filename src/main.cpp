4d37d5c0b52c62f2a585f761b5f3b840e1ad30ba
Rebecca Bracho <rebeccabracho05@gmail.com>
2023-10-19 13:03:10 -0400

    feat: initial structure

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+struct datos{
+    int id;
+    string movie;
+    string genders;
+    double duration;
+    string director;
+    string release_on;
+    string rent_to;
+    string rent_on;
+    string status;
+}peliculas[2000];
+
+
+int main(){
+
+}
